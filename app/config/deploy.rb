6c5b4e322ec9e3f070e32ea28e880d0bf0bc7b6c
Randolph Roble <roblerm@gmail.com>
2016-06-07 16:52:56 +0800

    update cap

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,9 +1,11 @@
 set :application, "pse-tool"
-set :domain,      "#{application}.com"
-set :deploy_to,   "/var/www/#{domain}"
+set :user, "ubuntu"
+set :domain,      "freetier2"
+set :deploy_to,   "/var/www/#{application}"
 set :app_path,    "app"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :repository,  "git@github.com:gapzroble/pse-tool.git"
+set :branch, "master"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
@@ -13,7 +15,21 @@ set :model_manager, "doctrine"
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
-set  :keep_releases,  3
+set  :keep_releases,  1
 
 # Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+logger.level = Logger::MAX_LEVEL
+
+set :deploy_via,      :rsync_with_remote_cache
+set :shared_files,    ["app/config/parameters.yml"]
+set :shared_children, ["vendor", "var/logs"]
+set :use_composer,    true
+set :use_sudo,        false
+
+set :log_path, "var/logs"
+set :cache_path, "var/cache"
+set :symfony_console, "bin/console"
+set :symfony_vendors,       "vendors"
+set :composer_bin,          "/usr/local/bin/composer"
+
+#after "deploy", "deploy:cleanup"
